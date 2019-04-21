6840c6feea7dd75f2e22b0f321e296caceaaa310
Ralf Northman <ralf.northman@bredband.net>
2019-04-21 13:49:33 +0200

    More documentation 5.

--- src/ZoomPlot.elm
+++ src/ZoomPlot.elm
@@ -108,7 +108,27 @@ defaultFormat number =
         , LineChart.line Colors.goldLight Dots.diamond "polysin" myPolySinData
         ]
 
-    _Note:_ This package use the fork [peterszerzo/line-charts](https://package.elm-lang.org/packages/peterszerzo/line-charts/latest) since it contains necessary updates from Tereza Sokols (terezka) original package. I advise you do the same if you don't want unnecessary duplication among your dependencies.
+    _Note:_ This package use the fork [peterszerzo/line-charts](https://package.elm-lang.org/packages/peterszerzo/line-charts/latest) since it contains necessary updates to Tereza Sokols (terezka) great original package. I advise you to do the same, otherwise namespace collisions seem inevitable.
+
+  - **xAcc** :
+    Getter function from your data type to the float value used for x coordinates.
+
+        .time >> Time.posixToMillis >> toFloat
+
+  - **yAcc** :
+    Getter function from your data type to the float value used for y coordinates.
+
+        .rocketThrust
+
+  - **pointDecoder** :
+    The internals of the package needs a way to convert Point back to your type. You basically have to write a function that puts the coordinates of a `Point` into an "empty" instance of your type.
+
+        \{ x, y } -> RocketData 0 0 "" x 0 "" 0 y Nothing
+
+  - **xIsTime** :
+    Wheter the x-axis should display its values as time or not.
+
+        True
 
 -}
 type alias Config data =
